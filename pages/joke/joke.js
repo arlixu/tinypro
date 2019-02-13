eee1c8b7b3e78a6248748853ccd22d3b1fe6ece2
xuyali <289964653@qq.com>
2019-02-13 09:07:42 +0800

    点赞和收藏1

--- pages/joke/joke.js
+++ pages/joke/joke.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    joke: []
+    joke: ["123","345"]
   },
   copyJoke:function(event){
     wx.setClipboardData({
@@ -31,7 +31,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.addJoke();
+    // this.addJoke();
   },
 
   /**
